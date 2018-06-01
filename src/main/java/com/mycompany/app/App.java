eb2dfedcf98cf85de86e678287916cbcc30d99e1
sanzhar73ismailov <sanzhar73@gmail.com>
2018-06-01 08:08:47 +0200

    2nd commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,11 +1,13 @@
 package com.mycompany.app;
 
+import java.util.Date;
+
 /**
  *
  * @author admin
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hell world!");
+        System.out.println("Hell world! " + new Date());
     }
 }
